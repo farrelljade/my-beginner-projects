8c4e777f153ab679469f1047ceaba10ea70b55d6
farrelljade <farrellmikel@yahoo.com>
2023-11-06 22:10:55 +0000

    Add inheritance class. Add user interactions to withdraw and transfer functions

--- bank_account_manager.py/bank_account.py
+++ bank_account_manager.py/bank_account.py
@@ -11,12 +11,12 @@ class BankAccount:
     
     def check_balance(self):
         # Display the current bank balance of the account holder
-        print(f"\n{self.acc_name}'s current balance is: £{self.balance}")
+        print(f"\nYour current balance is: £{self.balance}")
 
     def deposit(self, deposit_amount):
         # deposit funds into the account and update the balance
         self.balance += deposit_amount
-        print(f"\nDepositing £{deposit_amount:.2f}...\nDeposit complete.\nAccout balance: £{self.balance:.2f}.")
+        print(f"\nDepositing £{deposit_amount:.2f}...\nDeposit complete.\nYour accout balanceis now: £{self.balance:.2f}.")
 
     def enough_funds(self, amount):
         if self.balance >= amount:
@@ -29,8 +29,13 @@ class BankAccount:
         try:
             # checking/withdrawing funds
             self.enough_funds(amount)
-            self.balance -= amount
-            print(f"\nWithdrawing £{amount:.2f}...\nWithdrawal complete.\nAccount balance: £{self.balance:.2f}.")
+            print(f"\nWithdrawing £{amount:.2f}...")
+            user_choice = input("Confirm withdrawal: ").lower()
+            if user_choice in ["yes", "y"]:
+                self.balance -= amount
+                print(f"Withdrawal complete.\nYour account balance is now: £{self.balance:.2f}.")
+            else:
+                print(f"Withdrawal request cancelled.\nAccount balance: £{self.balance}")
         except FundsException as error:
             # handle cases of insufficient funds using our exception class
             print(f"\nAttempting to withdraw £{amount:.2f}...\n{error}")
@@ -39,10 +44,40 @@ class BankAccount:
         try:
             # first check enough funds to make the transfer
             self.enough_funds(amount)
-            user.balance += amount  # update the recipients account
-            self.balance -= amount  # adjust senders account
-            print(f"\nTransferring £{amount:.2f} to {user.acc_name}'s account...\nTransfer complete.")
-            print(f"Account balance: £{self.balance:.2f}")
+            print(f"\nTransferring £{amount:.2f} to {user.acc_name}'s account.")
+            user_choice = input("Confirm Transfer: ").lower()
+            if user_choice in ["yes", "y"]:
+                user.balance += amount  # update the recipients account
+                self.balance -= amount  # adjust senders account
+                print(f"Transfer complete.\nYour account balance is now: £{self.balance:.2f}")
+            else:
+                print(f"Transfer cancelled.\nAccount balance: {self.balance}")
         except FundsException as error:
             # handle cases of insufficient funds using our exception class
             print(f"Attempting to transfer £{amount:.2f} to {user}' account...\n{error}.")
+
+class SavingsAccount(BankAccount):
+
+    def deposit(self, amount):
+        self.interest = amount * 1.10
+        self.balance += self.interest
+        print(f"\nDepositing £{amount:.2f}...\nDeposit complete.\nYour accout balance is now: £{self.balance:.2f}.")
+
+class CashIsaAccount(SavingsAccount):
+    def __init__(self, acc_name, balance):
+        super().__init__(acc_name, balance)
+        self.withdraw_fee = 5
+
+    def withdraw(self, amount):
+        try:
+            self.enough_funds(amount)            
+            print(f"\nWithdrawal amount: £{amount:.2f}.")
+            user_choice = input(f"£{self.withdraw_fee} fee for this withdrawal. Do you accept the fee: ").lower()
+            if user_choice in ["yes", "y"]:
+                self.balance -= (amount + self.withdraw_fee)
+                print(f"Withdrawal complete.\nYour account balance is now: £{self.balance:.2f}.")
+            else:
+                print(f"Withdrawal request cancelled.\nAccount balance: £{self.balance}")
+        except FundsException as error:
+            # handle cases of insufficient funds using our exception class
+            print(f"\nAttempting to withdraw £{amount:.2f}...\n{error}")
