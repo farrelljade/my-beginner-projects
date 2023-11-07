907862e1cf7183825db49a3a4b171f883dfe2e38
farrelljade <farrellmikel@yahoo.com>
2023-11-07 21:58:14 +0000

    Add dict to class to store acc names/balances, reworked transfer function to allow user prompts

--- bank_account_manager.py/bank_account.py
+++ bank_account_manager.py/bank_account.py
@@ -3,58 +3,118 @@ class FundsException(Exception):
     pass
 
 class BankAccount:
+    user_accounts = {}
     # initialise bank account with account name and balance
-    def __init__(self, acc_name, balance):
+    def __init__(self, acc_name, balance=0):
         self.acc_name = acc_name
         self.balance = balance
-        print(f"\nBank account created for: {acc_name}\nAcoount balance: £{balance:.2f}.")
+        self.user_accounts[acc_name] = self  # add the instance to user_account dict
+        print(f"\nBank account created for: {acc_name}.")
+        self.bank_menu()
+            
     
+    # Bank menu added for more interaction
+    def bank_menu(self):
+        user_choice = input("\nOPTIONS:\n1. Check Balance\n2. Deposit\n3. Withdrawal\n4. Transfer\n5. Return Card\n\n").lower()
+        if user_choice in ["5", "return card", "returncard"]:
+            return
+        elif user_choice in ["1", "check balance", "checkbalance"]:
+            self.check_balance()
+        elif user_choice in ["2", "deposit"]:
+            self.deposit()
+        elif user_choice in ["3", "withdrawal"]:
+            self.withdraw()
+        elif user_choice in ["4", "transfer"]:
+            self.transfer()
+    
+    # Added function for user to go back to main menu or exit
+    def back_to_menu(self):
+        user_choice = input("\n1. Main menu\n2. Return card\n\n").lower()
+        if user_choice in ["1", "main menu", "mainmenu"]:
+            return self.bank_menu()
+        else:
+            return
+    
+    # Display the current bank balance of the account holder
     def check_balance(self):
-        # Display the current bank balance of the account holder
-        print(f"\nYour current balance is: £{self.balance}")
+        print(f"\n{self.acc_name}, your current balance is: £{self.balance:.2f}")
+        user_choice = input("1. Back to menu\n2. Return card\n\n").lower()
+        if user_choice == "1":
+            self.bank_menu()
+        else:
+            return
+    
+    # Deposit funds into the account and update bank balance
+    def deposit(self):
+        user_choice = float(input("\nDeposit amount: "))
+        self.balance += user_choice
+        print(f"\nDepositing £{user_choice:.2f}...\nDeposit complete.\n{self.acc_name}'s balance is now: £{self.balance:.2f}.")
+        self.back_to_menu()
 
-    def deposit(self, deposit_amount):
-        # deposit funds into the account and update the balance
-        self.balance += deposit_amount
-        print(f"\nDepositing £{deposit_amount:.2f}...\nDeposit complete.\nYour accout balanceis now: £{self.balance:.2f}.")
-
+    # Function to check enough funds before carrying out Withdrawals or Transfers
     def enough_funds(self, amount):
         if self.balance >= amount:
             return
         else:
-            # checking there are enough funds using the exception class
             raise FundsException(f"Insufficient funds.\nAccount balance: £{self.balance:.2f}.")
-
-    def withdraw(self, amount):
+    
+    # Withdraw function. Checks if enough funds in account before withdrawing
+    def withdraw(self):
+        withdraw_amount = 0
         try:
-            # checking/withdrawing funds
-            self.enough_funds(amount)
-            print(f"\nWithdrawing £{amount:.2f}...")
+            user_amount = input("\nWithdrawal amount:\n1. £10\n2. £20\n3. £50\n4. £100\n5. Other amount\n6. Return to menu\n\n")
+            if "." in user_amount:
+                print("Only £10 and £20 notes available.")
+                self.withdraw()
+            elif user_amount == "1":
+                withdraw_amount = 10
+            elif user_amount == "2":
+                withdraw_amount = 20
+            elif user_amount == "3":
+                withdraw_amount = 50
+            elif user_amount == "4":
+                withdraw_amount = 100
+            elif user_amount == "5":
+                withdraw_amount = int(input("How much do you want to withdraw: "))
+            elif user_amount in ["6", "return to menu", "returntomenu"]:
+                return self.bank_menu()
+            self.enough_funds(withdraw_amount)
+            print(f"\nWithdrawing £{withdraw_amount:.2f}...")
             user_choice = input("Confirm withdrawal: ").lower()
             if user_choice in ["yes", "y"]:
-                self.balance -= amount
-                print(f"Withdrawal complete.\nYour account balance is now: £{self.balance:.2f}.")
+                self.balance -= withdraw_amount
+                print(f"Withdrawal complete.\n{self.acc_name}'s account balance is now: £{self.balance:.2f}.")
+                self.back_to_menu()
             else:
-                print(f"Withdrawal request cancelled.\nAccount balance: £{self.balance}")
+                print(f"Withdrawal request cancelled.\n{self.acc_name}'s account balance: £{self.balance}")
         except FundsException as error:
             # handle cases of insufficient funds using our exception class
-            print(f"\nAttempting to withdraw £{amount:.2f}...\n{error}")
-
-    def transfer(self, user, amount):
-        try:
-            # first check enough funds to make the transfer
-            self.enough_funds(amount)
-            print(f"\nTransferring £{amount:.2f} to {user.acc_name}'s account.")
-            user_choice = input("Confirm Transfer: ").lower()
-            if user_choice in ["yes", "y"]:
-                user.balance += amount  # update the recipients account
-                self.balance -= amount  # adjust senders account
-                print(f"Transfer complete.\nYour account balance is now: £{self.balance:.2f}")
-            else:
-                print(f"Transfer cancelled.\nAccount balance: {self.balance}")
-        except FundsException as error:
+            print(f"\nAttempting to withdraw £{withdraw_amount:.2f}...\n{error}")
+            self.withdraw()
+    
+    # Transfer function using a dict (user_accounts) to check recipient has a bank account
+    def transfer(self):
+        user_amount = int(input(f"\nHi {self.acc_name}. How much do you want to transfer amount: "))
+        recipient_name = input("Whose account to transfer to: ")
+        recipient = self.user_accounts.get(recipient_name)
+        if recipient is not None:  # check in user is in the dict
+            try:
+                self.enough_funds(user_amount)
+                print(f"\nTransferring £{user_amount:.2f}...")
+                user_choice = input("Confirm transfer: ").lower()
+                if user_choice in ["yes", "y"]:
+                    recipient.balance += user_amount
+                    self.balance -= user_amount
+                    print(f"Transfer complete.\nYour account balance is now: £{self.balance:.2f}.")
+                    self.back_to_menu()
+                
+                else:
+                    print(f"Transfer cancelled.\nAccount balance: {self.balance}")
+            except FundsException as error:
             # handle cases of insufficient funds using our exception class
-            print(f"Attempting to transfer £{amount:.2f} to {user}' account...\n{error}.")
+                print(f"Attempting to transfer £{user_amount:.2f} to {recipient_name}'s account...\n{error}.")
+        else:
+            print("Recipient not found.")
 
 class SavingsAccount(BankAccount):
 
--- bank_account_manager.py/main.py
+++ bank_account_manager.py/main.py
@@ -1,1 +1,4 @@
-import bank_account
+from bank_account import BankAccount
+
+# bank_acc1 = BankAccount("bank_acc1")
+# bank_acc2 = BankAccount("bank_acc2")
