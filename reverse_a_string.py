e44ca336458454cbbbf31d54ed2286bf101cfe42
farrelljade <farrellmikel@yahoo.com>
2023-11-12 13:12:40 +0000

    Minor change

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -1,8 +1,8 @@
 # User to enter a sentence. The sentence should then be reversed and printed out.
 
-user = input("Please enter a sentence:\n\n")
+user = input("\nEnter your message:\n")
 
 # Here we slice the string. [::-1] results in the string being reversed from the last character.
 reversed_string = user[::-1]
 
-print(reversed_string)
+print(f"\nYour message reversed is:\n{reversed_string}")
