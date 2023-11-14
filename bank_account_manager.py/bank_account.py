84c05613eb5ad5d6023375fef407f616e8f926ac
farrelljade <farrellmikel@yahoo.com>
2023-11-14 18:42:00 +0000

    Add ability to switch between users whilst program running

--- bank_account_manager.py/bank_account.py
+++ bank_account_manager.py/bank_account.py
@@ -37,18 +37,19 @@ class BankAccount:
         if self.balance >= amount:
             self.balance -= amount
         else:
-            raise FundsException("Insufficient funds")
+            raise FundsException(f"Insufficient funds. Account balance: £{self.balance}")
 
     def transfer(self, recipient, amount):
         if self.balance >= amount:
             self.balance -= amount
             recipient.deposit(amount)
         else:
-            raise FundsException("Insufficient funds")
+            raise FundsException(f"Insufficient funds. Account balance: £{self.balance}")
 
 class UserInterface:
     def __init__(self):
         self.user_accounts = {}
+        self.recip_acc = None
     
     # whenever a new bank account created its added to the dict
     def create_account(self, acc_name, balance=0):
@@ -64,6 +65,13 @@ class UserInterface:
         user_choice = input("\n1. Main menu\n2. Return card\nEnter your choice: ").lower()
         if user_choice == "2" or user_choice == "return card":
             raise ReturnCard
+        
+    def switch_acc(self, recip_acc):
+        user_acc = input("\nWhat account to switch to: ")
+        if user_acc in self.user_accounts:
+            return user_acc
+        else:
+            print(f"{user_acc} not found.")
 
     def bank_menu(self, acc_name):
         while True:
@@ -73,6 +81,7 @@ class UserInterface:
             print("3. Withdrawal")
             print("4. Transfer")
             print("5. Return Card")
+            print("6. Switch Account")
 
             user_choice = input("Enter your choice: ").lower()
             if user_choice in ["5", "return card", "returncard"]:
@@ -100,11 +109,14 @@ class UserInterface:
                     amount = float(input("\nTransfer amount: "))
                     try:
                         account.transfer(recipient, amount)
-                        print(f"Transfer complete. {acc_name}'s balance is now: £{account.check_balance():.2f}")
+                        print(f"Transfer complete.\n{acc_name}'s balance is now: £{account.check_balance():.2f}")
+                        print(f"{recipient_name}'s balance is now: £{recipient.balance:.2f}")
                     except FundsException as error:
                         print(f"Attempting to transfer £{amount}...\n{error}")
                 else:
                     print("Recipient not found.")
+            elif user_choice in ["6", "switch account", "switchaccount"]:
+                self.switch_acc(self.recip_acc)
 
 ui = UserInterface()
 
